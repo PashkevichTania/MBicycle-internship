43b6106d229e2dafbbe7c7fd0a1ec76b995fdc32
TaniaPashkevich <pashkevichtania2004@gmail.com>
2021-10-06 15:12:22 +0300

    feat: add project edit

--- Time-Tracker/src/components/Projects/ProjectDetails.tsx
+++ Time-Tracker/src/components/Projects/ProjectDetails.tsx
@@ -1,6 +1,7 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {ProjectsContext} from 'components/Context/projectsProvider';
 import {IProject} from 'Interfaces';
+import ProjectEditForm from 'components/Projects/ProjectEditForm';
 import styles from 'components/Projects/Project.module.scss'
 
 const ProjectDetails = () => {
@@ -9,42 +10,54 @@ const ProjectDetails = () => {
     const url = window.location.href.toString().match(/project\/(.*)/gm);
     const id = url?.toString().substring(8);
     const project:IProject = projectsContext.projects.find((project: IProject)=> project.id == id );
+    const [show,setShow] = useState('none');
+
+    const showHandler = () => {
+        setShow('block')
+    }
+
+
 
     return (
         <div className={styles.projectDetails}>
             {project? (
-                <div className={styles.box}>
-                    <p className={styles.ID}>
+                <div>
+                    <div style={{display: show}} className={styles.popup}>
+                        <ProjectEditForm projectID={project.id} setShow={setShow} />
+                    </div>
+                    <div className={styles.box}>
+                        <p className={styles.ID}>
                         <span className={styles.span}>
                         Project ID:
                         </span>
-                        {project.id}
-                    </p>
-                    <p className={styles.user}>
+                            {project.id}
+                        </p>
+                        <p className={styles.user}>
                         <span className={styles.span}>
                         Project User:
                         </span>
-                        {project.user}
-                    </p>
-                    <p className={styles.name}>
+                            {project.user}
+                        </p>
+                        <p className={styles.name}>
                         <span className={styles.span}>
                         Project Name:
                         </span>
-                        {project.name}
-                    </p>
-                    <p className={styles.time}>
+                            {project.name}
+                        </p>
+                        <p className={styles.time}>
                         <span className={styles.span}>
                         Project time:
                         </span>
-                        {project.time}
-                    </p>
-                    <p className={styles.note}>
+                            {project.time}
+                        </p>
+                        <p className={styles.note}>
                         <span className={styles.span}>
                         Project Note:
                         </span>
-                        {project.note}
-                    </p>
-                    <button className={styles.editBtn}>Edit</button>
+                            {project.note}
+                        </p>
+                        <button className={styles.editBtn} onClick={showHandler}>Edit</button>
+                    </div>
                 </div>
             ): 'project not found'}
         </div>
---
+++ Time-Tracker/src/components/Projects/ProjectEditForm.tsx
@@ -0,0 +1,93 @@
+import React, {useContext} from 'react';
+import {IProject} from 'Interfaces';
+import {ProjectsContext} from 'components/Context/projectsProvider';
+import styles from 'components/Projects/PopUp.module.scss';
+
+
+const ProjectEditForm = (props: { projectID: string, setShow: React.Dispatch<string> }) => {
+
+    const projectsContext = useContext(ProjectsContext);
+
+    const {projectID, setShow} = props;
+    const currentProject = projectsContext.projects.find((p:IProject)=>{
+        return p.id === projectID;
+    })
+
+    const closeHandler = () => {
+        setShow('none')
+    }
+
+    const formSubmitHandler = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+
+        const target = event.target as typeof event.target & {
+            user: { value: string };
+            projectName: { value: string };
+            projectNote: { value: string };
+            projectTime: { value: string };
+        };
+
+        const newProject: IProject = {
+            id: projectID,
+            user: target.user.value,
+            name: target.projectName.value,
+            note: target.projectNote.value,
+            time: target.projectTime.value,
+        }
+
+        const updatedProjects = projectsContext.projects.map((p: IProject)=> {
+            return (p.id === newProject.id) ? newProject : p;
+        })
+        console.log(updatedProjects);
+        projectsContext.setProjects(updatedProjects);
+        closeHandler();
+    }
+    console.log("styles", styles)
+
+    return (
+        <div className={styles.form}>
+            <form onSubmit={formSubmitHandler}>
+                <div>
+                    <label htmlFor="user">Choose a user:</label>
+                    <input
+                        defaultValue={currentProject.user}
+                        required
+                        name="user"
+                        type="text"
+                        placeholder={"User"}
+                    />
+                </div>
+                <div className={styles.section}>
+                    <label htmlFor="projectName">Project name:</label>
+                    <input
+                        defaultValue={currentProject.name}
+                        required
+                        name="projectName"
+                        type="text"
+                        placeholder={"Project name"}/>
+                </div>
+                <div className={styles.section}>
+                    <label htmlFor="projectNote">Project note:</label>
+                    <textarea
+                        defaultValue={currentProject.note}
+                        name="projectNote"
+                        placeholder={"Project note"}/>
+                </div>
+                <div className={styles.section}>
+                    <label htmlFor="projectTime">Project time:</label>
+                    <input
+                        defaultValue={currentProject.time}
+                        name="projectTime"
+                        type="text"
+                        placeholder={"Project time"}/>
+                </div>
+                <div className={styles.btnContainer}>
+                    <button className={styles.closeBtn} onClick={closeHandler}>Cancel</button>
+                    <button className={styles.saveBtn} type={"submit"}>Save</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default ProjectEditForm;
