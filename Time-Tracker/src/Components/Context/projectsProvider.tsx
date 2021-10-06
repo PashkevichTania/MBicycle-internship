e79608728078bb0d90e8ea29931f144e76d321b9
TaniaPashkevich <pashkevichtania2004@gmail.com>
2021-10-06 11:12:46 +0300

    feat: add localstorage for projects

--- Time-Tracker/src/Components/Context/projectsProvider.tsx
+++ Time-Tracker/src/Components/Context/projectsProvider.tsx
@@ -1,12 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {IProject} from "../../Interfaces";
 
 export const ProjectsContext = React.createContext<any>({});
 
 const ProjectsProvider = (props: { children: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
 
-
+    const projectStorage = window.localStorage;
     const [projects, setProjects] = useState<IProject[]>([]);
+    useEffect(()=>{
+        if (projects.length){
+            projectStorage.setItem('projects', JSON.stringify(projects))
+        }
+    },[projects])
+    useEffect(()=>{
+        let projectsFromStorage = projectStorage.getItem('projects')
+        console.log("projectsFromStorage",  projectsFromStorage)
+        if (projectsFromStorage){
+            console.log("parse: ", JSON.parse(projectsFromStorage))
+            if (projectsFromStorage.length){
+                setProjects(JSON.parse(projectsFromStorage))
+            }
+        }
+    },[])
 
     return (
         <ProjectsContext.Provider value={{projects: projects, setProjects: setProjects}}>
